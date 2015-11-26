2f55076fefae1435cc95f19d13edf90122345616
peterdd <ddpm@liscovius.de>
2015-11-26 10:17:28 +0100

    Update bootstrap.php

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -10,9 +10,9 @@ require $file;
 spl_autoload_register('loader');
 */
 
-if(is_readable('../vendor/autoload.php')){
+if(is_readable('vendor/autoload.php')){
   // Use composer autoloader
-  require '../vendor/autoload.php';
+  require 'vendor/autoload.php';
 }
 
 #require_once 'includes/class.database.php';
