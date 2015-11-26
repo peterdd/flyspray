e79ae4448f723619830e68f68161e970e4370cac
peterdd <ddpm@liscovius.de>
2015-11-26 09:54:03 +0100

    Update flysprayTest.php

--- Tests/flysprayTest.php
+++ Tests/flysprayTest.php
@@ -3,7 +3,8 @@ class FlysprayTest extends PHPUnit_Framework_TestCase{
   public $db;
 
   public function setUp(){
-    $this->db = new Database($GLOBALS['dbhost'], $GLOBALS['dbuser'], $GLOBALS['dbpass'], $GLOBALS['dbname'], $GLOBALS['dbtype'], $GLOBALS['dbprefix']);
+    $this->db = new Database;
+    $this->db->dbOpen($GLOBALS['dbhost'], $GLOBALS['dbuser'], $GLOBALS['dbpass'], $GLOBALS['dbname'], $GLOBALS['dbtype'], $GLOBALS['dbprefix']);
     $this->db->Query("CREATE TABLE {projects} (what VARCHAR(50) NOT NULL)");
   }
   
